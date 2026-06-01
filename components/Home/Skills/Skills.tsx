cc5e5e1f6f22f810de30c94c2fc4470adc2ab1e9
Camden-Kirkpatrick <camdenckirkpatrick@gmail.com>
2026-06-01 16:47:35 -0500

    added skills with parallax effect

--- components/Home/Skills/Skills.tsx
+++ components/Home/Skills/Skills.tsx
@@ -1,9 +1,59 @@
-import React from 'react'
+'use client';
+import React from 'react';
+import { SiJavascript, SiNextdotjs, SiNodedotjs, SiReact, SiTailwindcss, SiTypescript } from 'react-icons/si';
+import Tilt from 'react-parallax-tilt';
+
+const skills = [
+    {
+        name: 'JavaScript',
+        icon: <SiJavascript/>
+    },
+    {
+        name: 'React JS',
+        icon: <SiReact />
+    },
+    {
+        name: 'Next.js',
+        icon: <SiNextdotjs />
+    },
+    {
+        name: 'Node.js',
+        icon: <SiNodedotjs />
+    },
+    {
+        name: 'Tailwind CSS',
+        icon: <SiTailwindcss />
+    },
+    {
+        name: 'TypeScript',
+        icon: <SiTypescript />
+    },
+]
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <div className='text-white pt-16 pb-16'>
+        <h1 className='text-center text-2xl md:text-4xl xl:text-5xl font-bold text-white'>
+            My <span className='text-cyan-300'>Skills</span>
+        </h1>
+        <div className='flex flex-wrap justify-center gap-6 mt-16'>
+            {skills.map((skill) => {
+                return (
+                    <Tilt key={skill.name} scale={1.5} transitionSpeed={400}>
+                        <div className="bg-[#14134145] text-center w-40 h-48 rounded-3xl flex flex-col items-center justify-center shadow-lg transition hover:scale-105">
+                            <div className='text-5xl mb-4 text-gray-300'>
+                                {skill.icon}
+                            </div>
+                            <p className='text-purple-400 mt-1'>
+                                {skill.name}
+                            </p>
+                        </div>
+                    </Tilt>
+                )
+            })}
+        </div>
+    </div>
   )
 }
 
-export default Skills
+export default Skills;
