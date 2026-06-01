6a175fd4d8d4c0907d0cd368a6162e8109dc481f
Camden-Kirkpatrick <camdenckirkpatrick@gmail.com>
2026-06-01 18:20:52 -0500

    added Resume animation

--- components/Home/Resume/Resume.tsx
+++ components/Home/Resume/Resume.tsx
@@ -12,7 +12,11 @@ const Resume = () => {
                 <h1 className='text-3xl sm:text-4xl font-bold text-white'>
                     My Work <span className='text-cyan-200'>Experience</span>
                 </h1>
-                <div className='mt-10'>
+                <div
+                    className='mt-10'
+                    data-aos='zoom-in'
+                    data-aos-anchor-placement='top-center'
+                >
                     <ResumeCard
                         Icon={FaCodepen}
                         role="Deli Clerk" 
@@ -30,7 +34,12 @@ const Resume = () => {
                 <h1 className='text-3xl sm:text-4xl font-bold text-white'>
                     My <span className='text-cyan-200'>Education</span>
                 </h1>
-                <div className='mt-10'>
+                <div
+                    className='mt-10'
+                    data-aos='zoom-out'
+                    data-aos-anchor-placement='top-center'
+                    data-aos-delay="300"
+                >
                     <ResumeCard 
                         Icon={BiBadge}
                         role="Associate of Applied Science, in Computer Information Systems"
